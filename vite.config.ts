b6ccf84a36392dc14854f3e9c09193449660690a
Korovin-Aleksandr <korovins94@yandex.ru>
2025-09-02 14:51:57 +0300

    поменял vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? 'https://github.com/Korovin-Aleksandr/Skilla' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://korovin-aleksandr.github.io' : '/',
   resolve: {
     alias: {
       '@shared': resolve(__dirname, 'src/shared'),
